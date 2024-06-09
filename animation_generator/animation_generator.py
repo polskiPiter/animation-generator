dbccf92660df09f14c10598ab508e47df1c6d9a9
polskiPiter <piter73piotr@wp.pl>
2024-06-09 15:54:10 +0200

    Changing the method for saving files

--- animation_generator/animation_generator.py
+++ animation_generator/animation_generator.py
@@ -1,17 +1,27 @@
-def main():
+def save_ppm_file(image_resolution, path_to_file):
     header = "P3"
     comment = "# Testowy obraz (tło o kolorze turkusowym)."
-    resolution = (1920, 1080)
-    path_to_file = r"D:\Kodowanie\Projekty\Python\animation-generator\data\Testowy obraz.ppm"
+    max_value_for_color = 255
 
-    content = f"{header}\n{comment}\n{resolution[0]} {resolution[1]}\n255"
+    content = f"{header}\n{comment}\n{image_resolution[0]} {image_resolution[1]}\n{max_value_for_color}"
 
-    for _ in range(resolution[0] * resolution[1]):
+    for _ in range(image_resolution[0] * image_resolution[1]):
         content += "\n64 224 208"
-    
-    ppm_file = open(path_to_file, "w")
-    ppm_file.write(content)
-    ppm_file.close()
+
+    # Nowoczesna metoda obsługi plików w Python'ie:
+    with open(path_to_file, "w") as ppm_file:
+        ppm_file.write(content)
+
+def main():
+    resolution = (1920, 1080)
+    path = r"D:\Kodowanie\Projekty\Python\animation-generator\data\Testowy obraz.ppm"
+
+    save_ppm_file(resolution, path)
+
+    # Klasyczna metoda obsługi plików w Python'ie:
+    #ppm_file = open(path_to_file, "w")
+    #ppm_file.write(content)
+    #ppm_file.close()
 
 if __name__ == "__main__":
     main()
